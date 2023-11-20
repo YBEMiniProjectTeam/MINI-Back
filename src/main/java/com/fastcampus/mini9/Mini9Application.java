1e665724b4457deb5dec6bd4aaf2c49261cf82fd
Denny-Ko <donghar@naver.com>
2023-11-20 17:34:41 +0900

    initial commit

---
+++ src/main/java/com/fastcampus/mini9/Mini9Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.mini9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mini9Application.class, args);
+    }
+
+}

9ff6b4f5dbec38ba8372986026fe60005ccd07fc
jun <jundev21@gmail.com>
2023-11-21 19:02:47 +0900

    [feat] Location Getter 추가

--- src/main/java/com/fastcampus/mini9/domain/accommodation/entity/Location.java
+++ src/main/java/com/fastcampus/mini9/domain/accommodation/entity/Location.java
@@ -1,8 +1,10 @@
 package com.fastcampus.mini9.domain.accommodation.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
 
 @Embeddable
+@Getter
 public class Location {
 	private Region region;
 	private District district;

3b7206528d15a0a5a50a1a00ca3fe721a51fd112
jun <jundev21@gmail.com>
2023-11-23 12:13:54 +0900

    [feat] 객실 래퍼 추가

--- src/main/java/com/fastcampus/mini9/domain/room/controller/dto/RoomDtoMapper.java
+++ src/main/java/com/fastcampus/mini9/domain/room/controller/dto/RoomDtoMapper.java
@@ -1,4 +1,15 @@
 package com.fastcampus.mini9.domain.room.controller.dto;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface RoomDtoMapper {
+
+	RoomDtoMapper INSTANCE = Mappers.getMapper(RoomDtoMapper.class);
+
+	// Room fromEntity(RoomResDto roomResDto);
+
+	RoomResDto roomResDto(Long accommodationId, String startDate, String endDate,
+		String guestNum);
 }
--- src/main/java/com/fastcampus/mini9/domain/room/controller/dto/RoomResDto.java
+++ src/main/java/com/fastcampus/mini9/domain/room/controller/dto/RoomResDto.java
@@ -1,4 +1,13 @@
 package com.fastcampus.mini9.domain.room.controller.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class RoomResDto {
+	Long accommodationId;
+	String startDate;
+	String endDate;
+	String guestNum;
 }

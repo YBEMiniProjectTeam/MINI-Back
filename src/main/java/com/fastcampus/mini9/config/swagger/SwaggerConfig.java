e874f455d4375505e59be5f1d7dc47ade82bcfd2
Denny-Ko <donghar@naver.com>
2023-11-24 00:57:42 +0900

    [feat] Swagger configuration
    provide basic info about server & authentication module & example for signup request

---
+++ src/main/java/com/fastcampus/mini9/config/swagger/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.fastcampus.mini9.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+
+@OpenAPIDefinition(
+	// servers = {
+	// 	@Server(url = "https://anti-bias.kr/api", description = "Server")
+	// },
+	info = @Info(
+		title = "Accommodation Reservation Platform API",
+		description = "Accommodation Reservation Platform를 위한 API",
+		version = "v1",
+		contact = @Contact(name = "고동훤", email = "donghar@naver.com", url = "https://github.com/Dr-KoKo")
+	)
+)
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openApi() {
+		String authorizationHeader = "Authorization";
+		String refreshSchemeName = "refresh-token";
+
+		SecurityScheme jwtScheme = new SecurityScheme()
+			.name(authorizationHeader)
+			.type(Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT");
+		SecurityScheme refreshScheme = new SecurityScheme()
+			.name(refreshSchemeName)
+			.type(Type.APIKEY)
+			.in(In.COOKIE);
+		Components components = new Components()
+			.addSecuritySchemes(authorizationHeader, jwtScheme)
+			.addSecuritySchemes(refreshSchemeName, refreshScheme);
+
+		SecurityRequirement securityRequirement = new SecurityRequirement()
+			.addList(authorizationHeader)
+			.addList(refreshSchemeName);
+
+		return new OpenAPI()
+			.addSecurityItem(securityRequirement)
+			.components(components);
+	}
+}
--- src/main/java/com/fastcampus/mini9/domain/member/controller/dto/request/SignupRequestDto.java
+++ src/main/java/com/fastcampus/mini9/domain/member/controller/dto/request/SignupRequestDto.java
@@ -1,11 +1,13 @@
 package com.fastcampus.mini9.domain.member.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public record SignupRequestDto(
+    @Schema(example = "donghar@naver.com")
     @NotBlank(message = "이메일은 필수 입력 사항입니다.")
     @Email(message = "이메일 형식으로 입력해 주세요.")
     String email,

a5873de576d0005e80fb5bce5a6e7e4d779f7d28
jun <jundev21@gmail.com>
2023-11-23 12:14:22 +0900

    [feat] 객실 컨트롤러 구현

--- src/main/java/com/fastcampus/mini9/domain/room/controller/RoomController.java
+++ src/main/java/com/fastcampus/mini9/domain/room/controller/RoomController.java
@@ -1,9 +1,12 @@
 package com.fastcampus.mini9.domain.room.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fastcampus.mini9.domain.room.controller.dto.RoomResDto;
 import com.fastcampus.mini9.domain.room.service.RoomService;
 
 @RestController
@@ -11,23 +14,27 @@ public class RoomController {
 
 	private final RoomService roomService;
 
-	public RoomController() {
-		roomService = new RoomService();
+	private RoomController(RoomService roomService) {
+		this.roomService = roomService;
 	}
 
 	//숙소 기준 객실 정보
-	@GetMapping("/accommodations/{id}/rooms")
-	public void roomInfo(
-		@PathVariable Long accommodationId
+	@GetMapping("/accommodations/{accommodationId}/rooms")
+	public ResponseEntity<RoomResDto> roomsInfo(
+		@PathVariable Long accommodationId,
+		@RequestParam(value = "start_date") String startDate,
+		@RequestParam(value = "end_date") String endDate,
+		@RequestParam(value = "guest_num") String guestNum
 	) {
 
+		return roomService.roomsInfo(accommodationId, startDate, endDate, guestNum);
 	}
 
 	//객실 상세정보
 	@GetMapping("/rooms/{id}")
-	public void detailOfRooms(
+	public ResponseEntity<RoomResDto> detailRoom(
 		@PathVariable Long roomId
 	) {
-
+		return roomService.detailRoom(roomId);
 	}
 }

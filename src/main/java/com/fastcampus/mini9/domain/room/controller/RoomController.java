2b1d4ba73a65a01a2d218731699a1110853f2d4a
jun <jundev21@gmail.com>
2023-11-24 10:34:22 +0900

    [update] get 파라미터 date time formate 추가

--- src/main/java/com/fastcampus/mini9/domain/room/controller/RoomController.java
+++ src/main/java/com/fastcampus/mini9/domain/room/controller/RoomController.java
@@ -1,12 +1,17 @@
 package com.fastcampus.mini9.domain.room.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fastcampus.mini9.domain.room.controller.dto.RoomResDto;
+import com.fastcampus.mini9.domain.room.controller.dto.RoomResDtoList.RoomResDto;
+import com.fastcampus.mini9.domain.room.controller.dto.RoomSpecificDto;
 import com.fastcampus.mini9.domain.room.service.RoomService;
 
 @RestController
@@ -20,19 +25,19 @@ public class RoomController {
 
 	//숙소 기준 객실 정보
 	@GetMapping("/accommodations/{accommodationId}/rooms")
-	public ResponseEntity<RoomResDto> roomsInfo(
+	public ResponseEntity<List<RoomResDto>> roomsInfo(
 		@PathVariable Long accommodationId,
-		@RequestParam(value = "start_date") String startDate,
-		@RequestParam(value = "end_date") String endDate,
-		@RequestParam(value = "guest_num") String guestNum
+		@RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+		@RequestParam(value = "end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
+		@RequestParam(value = "guest_num") @DateTimeFormat(pattern = "yyyy-MM-dd") int guestNum
 	) {
 
 		return roomService.roomsInfo(accommodationId, startDate, endDate, guestNum);
 	}
 
 	//객실 상세정보
-	@GetMapping("/rooms/{id}")
-	public ResponseEntity<RoomResDto> detailRoom(
+	@GetMapping("/rooms/{roomId}")
+	public ResponseEntity<RoomSpecificDto> detailRoom(
 		@PathVariable Long roomId
 	) {
 		return roomService.detailRoom(roomId);

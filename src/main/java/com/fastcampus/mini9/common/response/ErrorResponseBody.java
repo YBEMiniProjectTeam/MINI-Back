bf3542e9c04842a2ef4d534cf9f5d74c840a091c
Denny-Ko <donghar@naver.com>
2023-11-24 23:06:18 +0900

    [feat] Remove exception field in ErrorResponseBody

--- src/main/java/com/fastcampus/mini9/common/response/ErrorResponseBody.java
+++ src/main/java/com/fastcampus/mini9/common/response/ErrorResponseBody.java
@@ -8,25 +8,18 @@ public class ErrorResponseBody {
     private final boolean isSuccessful;
     private final Integer statusCode;
     private final String message;
-    private Throwable ex;
 
-    public ErrorResponseBody(boolean isSuccessful, Integer statusCode, String message,
-        Throwable ex) {
+    public ErrorResponseBody(boolean isSuccessful, Integer statusCode, String message) {
         this.isSuccessful = isSuccessful;
         this.statusCode = statusCode;
         this.message = message;
-        this.ex = ex;
     }
 
     public static ErrorResponseBody unsuccessful(String message) {
-        return new ErrorResponseBody(false, 400, message, null);
-    }
-
-    public static ErrorResponseBody unsuccessful(String message, Throwable ex) {
-        return new ErrorResponseBody(false, 400, message, ex);
+        return new ErrorResponseBody(false, 400, message);
     }
 
     public static ErrorResponseBody unsuccessful(Integer statusCode, String message) {
-        return new ErrorResponseBody(false, statusCode, message, null);
+        return new ErrorResponseBody(false, statusCode, message);
     }
 }

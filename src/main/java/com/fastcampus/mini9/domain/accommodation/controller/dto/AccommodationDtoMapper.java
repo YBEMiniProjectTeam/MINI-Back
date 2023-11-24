3e01b1a6ed85dc4759f6fdd10ba921250d79e95e
jun <jundev21@gmail.com>
2023-11-24 10:33:29 +0900

    [update] 숙박 데이터 수정

--- src/main/java/com/fastcampus/mini9/domain/accommodation/controller/dto/AccommodationDtoMapper.java
+++ src/main/java/com/fastcampus/mini9/domain/accommodation/controller/dto/AccommodationDtoMapper.java
@@ -2,13 +2,6 @@ package com.fastcampus.mini9.domain.accommodation.controller.dto;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.factory.Mappers;
-
-import com.fastcampus.mini9.domain.accommodation.entity.Accommodation;
-import com.fastcampus.mini9.domain.accommodation.entity.AccommodationImage;
-import com.fastcampus.mini9.domain.accommodation.entity.District;
-import com.fastcampus.mini9.domain.accommodation.entity.Location;
-import com.fastcampus.mini9.domain.accommodation.entity.Region;
 
 /**
  * 사용방법
@@ -18,15 +11,15 @@ import com.fastcampus.mini9.domain.accommodation.entity.Region;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AccommodationDtoMapper {
 
-	AccommodationDtoMapper INSTANCE = Mappers.getMapper(AccommodationDtoMapper.class);
-
-	AccommodationDto accommodationDto(Accommodation accommodation);
-
-	AccommodationImgDto accommodationImgDto(AccommodationImage accommodationImage);
-
-	LocationDto locationDto(Location location);
-
-	DistrictDto districtDto(District district);
-
-	RegionDto regionDto(Region region);
+	// AccommodationDtoMapper INSTANCE = Mappers.getMapper(AccommodationDtoMapper.class);
+	//
+	// AccommodationDto accommodationDto(Accommodation accommodation);
+	//
+	// AccommodationImgDto accommodationImgDto(AccommodationImage accommodationImage);
+	//
+	// LocationDto locationDto(Location location);
+	//
+	// DistrictDto districtDto(District district);
+	//
+	// RegionDto regionDto(Region region);
 }
--- src/main/java/com/fastcampus/mini9/domain/accommodation/entity/Accommodation.java
+++ src/main/java/com/fastcampus/mini9/domain/accommodation/entity/Accommodation.java
@@ -2,14 +2,17 @@ package com.fastcampus.mini9.domain.accommodation.entity;
 
 import java.util.List;
 
+import com.fastcampus.mini9.domain.room.entity.Room;
+
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,22 +26,26 @@ public class Accommodation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	@OneToMany
+	@OneToMany(mappedBy = "accommodation")
 	private List<AccommodationImage> accommodationImage;
 	@Enumerated(EnumType.STRING)
 	private AccommodationType type;
-	@ManyToOne
-	private Region region;
-	@ManyToOne
-	private District district;
+	@Embedded
+	private Location location;
+	@OneToMany(mappedBy = "accommodation")
+	private List<Room> rooms;
+	@OneToOne
+	private AccommodationDetailInfo accommodationDetailInfo;
 
 	@Builder
 	public Accommodation(String name, List<AccommodationImage> accommodationImage, AccommodationType type,
-		Region region, District district) {
+		Location location, List<Room> rooms, AccommodationDetailInfo accommodationDetailInfo) {
 		this.name = name;
 		this.accommodationImage = accommodationImage;
 		this.type = type;
-		this.region = region;
-		this.district = district;
+		this.location = location;
+		this.rooms = rooms;
+		this.accommodationDetailInfo = accommodationDetailInfo;
+
 	}
 }

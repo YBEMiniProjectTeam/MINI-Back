98989647cc14f766b18c2feeac626bb2bc060bd8
jun <jundev21@gmail.com>
2023-11-26 20:05:23 +0900

    [feat] member wishlist 추가

--- src/main/java/com/fastcampus/mini9/domain/member/entity/Member.java
+++ src/main/java/com/fastcampus/mini9/domain/member/entity/Member.java
@@ -1,10 +1,15 @@
 package com.fastcampus.mini9.domain.member.entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fastcampus.mini9.domain.wish.entity.Wish;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,19 +20,22 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String email;
-    private String pwd;
-    private String name;
-    private LocalDate birthday;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String pwd;
+	private String name;
+	private LocalDate birthday;
+	@OneToMany
+	private List<Wish> wishList;
 
-    @Builder
-    private Member(String email, String pwd, String name, LocalDate birthday) {
-        this.email = email;
-        this.pwd = pwd;
-        this.name = name;
-        this.birthday = birthday;
-    }
+	@Builder
+	private Member(String email, String pwd, String name, LocalDate birthday, List<Wish> wishList) {
+		this.email = email;
+		this.pwd = pwd;
+		this.name = name;
+		this.birthday = birthday;
+		this.wishList = wishList;
+	}
 }

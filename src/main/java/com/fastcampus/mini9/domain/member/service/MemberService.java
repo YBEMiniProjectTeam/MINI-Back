87933249bf2a34579e0d9ee7458e596d8ccb66b5
hyeond0 <a98792530@gmail.com>
2023-11-28 11:43:11 +0900

    [fix] @Transactional 추가

--- src/main/java/com/fastcampus/mini9/domain/member/service/MemberService.java
+++ src/main/java/com/fastcampus/mini9/domain/member/service/MemberService.java
@@ -9,6 +9,7 @@ import com.fastcampus.mini9.domain.member.service.dto.response.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public MemberDto save(MemberSaveDto dto) {
         if (memberRepository.findByEmail(dto.email()).isPresent()) {
             throw new ExistsArgumentException("이미 존재하는 이메일입니다.");
@@ -27,6 +29,7 @@ public class MemberService {
         return MemberDto.toDto(saveMember);
     }
 
+    @Transactional(readOnly = true)
     public MemberDto getProfile(String email) {
         Member member = findByEmail(email);
         return MemberDto.toDto(member);

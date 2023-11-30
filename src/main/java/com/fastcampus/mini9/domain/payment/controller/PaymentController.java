a8b9d5a5dd7fc1e725614e5e68e4bdd0566166e7
ktpark <qkrrudxkr77@naver.com>
2023-11-30 19:03:39 +0900

    [feat] 예약, 결제 취소

--- src/main/java/com/fastcampus/mini9/domain/payment/controller/PaymentController.java
+++ src/main/java/com/fastcampus/mini9/domain/payment/controller/PaymentController.java
@@ -2,11 +2,13 @@ package com.fastcampus.mini9.domain.payment.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fastcampus.mini9.common.response.BaseResponseBody;
 import com.fastcampus.mini9.common.response.DataResponseBody;
 import com.fastcampus.mini9.domain.payment.dto.FindAllPaymentResponse;
 import com.fastcampus.mini9.domain.payment.dto.FindDetailPaymentResponse;
@@ -32,5 +34,13 @@ public class PaymentController {
 	@GetMapping("/{paymentId}")
 	public DataResponseBody<FindDetailPaymentResponse> findDetail(@PathVariable Long paymentId) {
 		return DataResponseBody.success(paymentService.findDetail(paymentId));
+	}
+
+	@Operation(summary = "예약, 결제 취소")
+	@DeleteMapping("/{paymentId}")
+	public BaseResponseBody delete(@PathVariable Long paymentId) {
+		paymentService.delete(paymentId);
+
+		return DataResponseBody.success();
 	}
 }
